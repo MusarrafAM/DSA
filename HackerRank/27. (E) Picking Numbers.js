f330cf407e4376347a121b2b0a5135986a1e0b20
Musarraf <muhammedmusarraf@gmail.com>
2024-11-12 18:31:14 +0530

    Picking Numbers

---
+++ HackerRank/27. (E) Picking Numbers.js
@@ -0,0 +1,28 @@
+function pickingNumbers(a) {
+  // Write your code here
+
+  let PickedNumbersListOfList = [];
+  let MaxSizedListLength = 0;
+
+  a.sort((a, b) => a - b);
+
+  for (let i = 0; i < a.length; i++) {
+    let newList = [];
+    newList.push(a[i]);
+    for (let j = i + 1; j < a.length; j++) {
+      let difference = Math.abs(a[i] - a[j]);
+      if (difference <= 1) {
+        newList.push(a[j]);
+      }
+    }
+    PickedNumbersListOfList.push(newList);
+  }
+
+  PickedNumbersListOfList.forEach((arr) => {
+    if (arr.length > MaxSizedListLength) {
+      MaxSizedListLength = arr.length;
+    }
+  });
+
+  return MaxSizedListLength;
+}
