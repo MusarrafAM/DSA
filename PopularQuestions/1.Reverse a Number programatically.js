8402cdde0285d838ffaaa39f8821fc01b133b96d
Musarraf <muhammedmusarraf@gmail.com>
2024-11-17 11:15:44 +0530

    Added Populer question 1 reverse a number without any method

--- PopularQuestions/1.Reverse a Number programatically.js
+++ PopularQuestions/1.Reverse a Number programatically.js
@@ -1,15 +1,25 @@
-let number = 123456789;
+//! reverse a Number without using any methods, just by using pure programming.
+// Very Important Interview question (Asked by calcey)
+// let number = 123456789;
 
-// Initializing the result variable
-let result = 0;
+// // Initializing the result variable
+// let result = 0;
 
-while (number > 0) {
-  // Getting the rightmost digit
-  rightmost = number % 10;
+// while (number > 0) {
+//   // Getting the rightmost digit
+//   rightmost = number % 10;
 
-  result = result * 10 + rightmost;
+//   result = result * 10 + rightmost;
 
-  // Removing the rightmost digit from the number
-  number = Math.floor(number / 10);
-}
-console.log("Reversed number is : " + result);
+//   // Removing the rightmost digit from the number
+//   number = Math.floor(number / 10);
+// }
+// console.log("Reversed number is : " + result);
+
+//! Below is the easy way using combination methods()
+// let result = 0;
+// let number = 123456789;
+
+// result  = parseInt(number.toString().split("").reverse().join(""))
+
+// console.log("Reversed number is : "+result);
