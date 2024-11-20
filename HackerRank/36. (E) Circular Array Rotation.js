dcc7f696283b9dc65541d8e5f9d21bf1a299a77b
Musarraf <muhammedmusarraf@gmail.com>
2024-11-20 12:57:44 +0530

    add extra easy method to solve the Circular Array Rotation with unshift and pop

--- HackerRank/36. (E) Circular Array Rotation.js
+++ HackerRank/36. (E) Circular Array Rotation.js
@@ -1,21 +1,39 @@
+//! Method - 01 Very easy way with using unshift and pop
+// shift = remove from first index
+// unshift = add to first index
+// pop = remove from last index
+// push = add to last index
+
 function circularArrayRotation(a, k, queries) {
   // Write your code here
-  const arrayLength = a.length;
-  const newList = [];
-  const answer = [];
 
-  // Normalize k to avoid redundant rotations
-  //  This is Extra optimization without this also code Work
-  k = k % arrayLength;
-
-  for (let i = 0; i < arrayLength; i++) {
-    let afterRotationIndex = (i + k) % arrayLength;
-    newList[afterRotationIndex] = a[i];
+  for (let i = 0; i < k; i++) {
+    a.unshift(a.pop()); // All this do is remove the last element and add it to start index.
   }
 
-  queries.forEach((i) => {
-    answer.push(newList[i]);
-  });
-
-  return answer;
+  //Whenever we see a list need to be returned think of map method
+  return queries.map((queryIndex) => a[queryIndex]);
 }
+
+//! Method - 02 Hard way Regular method using modular
+// function circularArrayRotation(a, k, queries) {
+//   // Write your code here
+//   const arrayLength = a.length;
+//   const newList = [];
+//   const answer = [];
+
+//   // Normalize k to avoid redundant rotations
+//   //  This is Extra optimization without this also code Work
+//   k = k % arrayLength;
+
+//   for (let i = 0; i < arrayLength; i++) {
+//     let afterRotationIndex = (i + k) % arrayLength;
+//     newList[afterRotationIndex] = a[i];
+//   }
+
+//   queries.forEach((i) => {
+//     answer.push(newList[i]);
+//   });
+
+//   return answer;
+// }
