2c38b516cbeaec212e17ed90c0c58a99d9cf6b81
Musarraf <muhammedmusarraf@gmail.com>
2024-11-25 17:51:31 +0530

    Sherlock and Squares

---
+++ HackerRank/42. (E) Sherlock and Squares.js
@@ -0,0 +1,10 @@
+// if we try using for loop it will exceed the time limit
+
+function squares(a, b) {
+  // Write your code here
+
+  let startingSquareNumber = Math.ceil(Math.sqrt(a));
+  let endSquareNumber = Math.floor(Math.sqrt(b));
+
+  return endSquareNumber - startingSquareNumber + 1;
+}
