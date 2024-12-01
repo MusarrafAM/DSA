68fdb3084ed9ec761d61c07a31c71505e8069dd0
Musarraf <muhammedmusarraf@gmail.com>
2024-12-01 14:58:50 +0530

    Jumping on the Clouds

---
+++ HackerRank/47. (E) Jumping on the Clouds.js
@@ -0,0 +1,17 @@
+function jumpingOnClouds(c) {
+  // Write your code here
+  let minNumOfJumpsReq = 0;
+
+  let currPosition = 0;
+  while (currPosition < c.length - 1) {
+    if (c[currPosition + 2] !== 1) {
+      currPosition += 2;
+    } else {
+      currPosition++;
+    }
+
+    minNumOfJumpsReq++;
+  }
+
+  return minNumOfJumpsReq;
+}
