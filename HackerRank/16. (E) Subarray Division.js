8120fdb34589d40675986f7e1fafcdac595dc579
Musarraf <muhammedmusarraf@gmail.com>
2025-01-03 11:56:49 +0530

    Add new way of solving the Subarray Division using 2 pointer sliding window

--- HackerRank/16. (E) Subarray Division.js
+++ HackerRank/16. (E) Subarray Division.js
@@ -1,3 +1,30 @@
+//! Recently completed it using 2 pointers sliding window (Did it on the first run - felt awesome)
+function birthday(s, d, m) {
+  // Write your code here
+  // length = m
+  // sum Integers = d
+
+  let numOfWays = 0;
+
+  let leftPointer = 0;
+  let rightPointer = m - 1;
+
+  while (rightPointer <= s.length) {
+    let sum = 0;
+    for (let i = leftPointer; i <= rightPointer; i++) {
+      sum += s[i];
+    }
+    if (sum === d) {
+      numOfWays++;
+    }
+
+    leftPointer++;
+    rightPointer++;
+  }
+
+  return numOfWays;
+}
+
 //! Simple way = creating sub array in each iteration.
 
 // function birthday(s, d, m) {
@@ -25,14 +52,13 @@
 // }
 
 //! Sliding window Technique (This is not exacly a sliding window, but kind of..)
-function birthday(s, d, m) {
-  // Write your code here
-  // d = toatl value
-  // m = consecutive bar
+// function birthday(s, d, m) {
+//   // Write your code here
+//   // d = toatl value
+//   // m = consecutive bar
 
-  let count = 0;
+//   let count = 0;
 
-  
-}
+// }
 
-console.log(birthday((s = [1, 2, 1, 3, 2]), (d = 3), (m = 2)));
+// console.log(birthday((s = [1, 2, 1, 3, 2]), (d = 3), (m = 2)));
