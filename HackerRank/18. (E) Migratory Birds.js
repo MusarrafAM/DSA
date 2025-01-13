51fc5a5ee4d3aac14afd4f439d80990b2c2f3317
Musarraf <muhammedmusarraf@gmail.com>
2025-01-13 18:06:57 +0530

    Updated simple solution for day of prpgrammer

--- HackerRank/18. (E) Migratory Birds.js
+++ HackerRank/18. (E) Migratory Birds.js
@@ -1,7 +1,6 @@
-//! Very Very Important Do this frequently
+//! Very Very Important Do this frequently.
 
 //! My initial Solution took so much time cause had to learn object iteration.
-
 // function migratoryBirds(arr) {
 //   // Write your code here
 //   let typeAndCOunt = {};
@@ -31,34 +30,34 @@
 
 //! Solution 2 with new Array()  with a way of finding
 
-function migratoryBirds(arr) {
-  // Create a frequency array where the index corresponds to bird type
-  let frequency = new Array(6).fill(0); // Assuming bird types are between 1 and 5 (inclusive)
-  //   since array index starting from 0, we cant create 5, if create 5 we will ony det index upto 4 not the 5th,
-  //   thats why we create 6 element in the array so we can get the 1st and 5th element
-  //   we wont use 0th index element,it will we 0 forever.
+// function migratoryBirds(arr) {
+//   // Create a frequency array where the index corresponds to bird type
+//   let frequency = new Array(6).fill(0); // Assuming bird types are between 1 and 5 (inclusive)
+//   //   since array index starting from 0, we cant create 5, if create 5 we will ony det index upto 4 not the 5th,
+//   //   thats why we create 6 element in the array so we can get the 1st and 5th element
+//   //   we wont use 0th index element,it will we 0 forever.
 
-  // Count the occurrences of each bird type
-  arr.forEach((bird) => {
-    frequency[bird]++; // Increment the count for the bird type
-  });
+//   // Count the occurrences of each bird type
+//   arr.forEach((bird) => {
+//     frequency[bird]++; // Increment the count for the bird type
+//   });
 
-  // Find the bird type with the maximum frequency
-  let maxFrequency = 0;
-  let birdTypeWithMaxFrequency = -1;
+//   // Find the bird type with the maximum frequency
+//   let maxFrequency = 0;
+//   let birdTypeWithMaxFrequency = -1;
 
-  for (let i = 1; i < frequency.length; i++) {
-    if (frequency[i] > maxFrequency) {
-      maxFrequency = frequency[i];
-      birdTypeWithMaxFrequency = i; // Found a new bird with higher frequency
-    } else if (frequency[i] === maxFrequency) {
-      // If frequencies are the same, take the smaller bird ID = birdTypeWithMaxFrequency, i = comparing 2 num.
-      birdTypeWithMaxFrequency = Math.min(birdTypeWithMaxFrequency, i);
-    }
-  }
+//   for (let i = 1; i < frequency.length; i++) {
+//     if (frequency[i] > maxFrequency) {
+//       maxFrequency = frequency[i];
+//       birdTypeWithMaxFrequency = i; // Found a new bird with higher frequency
+//     } else if (frequency[i] === maxFrequency) {
+//       // If frequencies are the same, take the smaller bird ID = birdTypeWithMaxFrequency, i = comparing 2 num.
+//       birdTypeWithMaxFrequency = Math.min(birdTypeWithMaxFrequency, i);
+//     }
+//   }
 
-  return birdTypeWithMaxFrequency;
-}
+//   return birdTypeWithMaxFrequency;
+// }
 
 //! Solution 3 with just list
 function migratoryBirds(arr) {
--- HackerRank/19. (E) Day of the Programmer.js
+++ HackerRank/19. (E) Day of the Programmer.js
@@ -1,43 +1,103 @@
+//! new method easy to understand method
 function dayOfProgrammer(year) {
   // Write your code here
-  const first8MonthInNormalYear = 243;
-  const first8MonthInDaysLeapYear = 244;
-  const first8MonthInDays1918 = 230; // In 1918, due to the calendar transition, the first 8 months had 230 days   (Need to reduce 14 days = since after jan 31  is feb 14)
 
-  let date = 0;
-  let isLeapYear = false;
-  //find teh day of programmer 256. ====  256 - first 8 months (243) can get the september date.
+  // 1700 to 1917 = rusia Julian  calander
+  // from 1917 rusia used gregorian calander
+  // 1918 trasisin calander from julian to gregorian
+  // 1918  next day after January 31st was February 14th
+  // in 1918, February 14th was the 32nd day of the year in Russia.
 
-  function findLeapInGregorian(year) {
-    if (year % 400 == 0) {
+  // both calander 29 days in feb during leap year. 28 in norma year
+
+  // leapyear 256 = September 12
+  // not leap year = September 13
+
+  const isJulieanLeap = (year) => {
+    return year % 4 === 0;
+  };
+
+  const isGregorianLeap = (year) => {
+    if (year % 400 === 0) {
       return true;
-    } else if (year % 4 == 0 && year % 100 != 0) {
-      return true;
-    } else {
-      return false;
     }
-  }
 
-  // Leap year calculation
-  if (year >= 1700 && year <= 1917) {
-    // Julian calendar
     if (year % 4 === 0) {
-      isLeapYear = true;
+      if (year % 100 !== 0) {
+        return true;
+      }
+    }
+
+    return false;
+  };
+
+  let day = "";
+
+  if (year <= 1917) {
+    //jiliean
+    const isLeap = isJulieanLeap(year);
+    if (isLeap) {
+      day = "12";
+    } else {
+      day = "13";
     }
   } else if (year >= 1919) {
-    // Gregorian calendar
-    isLeapYear = findLeapInGregorian(year);
-  } else if (year === 1918) {
-    // Special case for 1918 (transition year)
-    // The 256th day in 1918 is different due to the shift in dates (256 - 230)
-    return `26.09.${year}`;
-  }
-
-  if (isLeapYear) {
-    date = 256 - first8MonthInDaysLeapYear;
+    //gregorian
+    const isLeap = isGregorianLeap(year);
+    if (isLeap) {
+      day = "12";
+    } else {
+      day = "13";
+    }
   } else {
-    date = 256 - first8MonthInNormalYear;
+    // 1918
+    day = "26";
   }
 
-  return `${date}.09.${year}`;
+  return `${day}.09.${year}`;
 }
+
+//! My prev Method, lots of calculations
+// function dayOfProgrammer(year) {
+//   // Write your code here
+//   const first8MonthInNormalYear = 243;
+//   const first8MonthInDaysLeapYear = 244;
+//   const first8MonthInDays1918 = 230; // In 1918, due to the calendar transition, the first 8 months had 230 days   (Need to reduce 14 days = since after jan 31  is feb 14)
+
+//   let date = 0;
+//   let isLeapYear = false;
+//   //find teh day of programmer 256. ====  256 - first 8 months (243) can get the september date.
+
+//   function findLeapInGregorian(year) {
+//     if (year % 400 == 0) {
+//       return true;
+//     } else if (year % 4 == 0 && year % 100 != 0) {
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   }
+
+//   // Leap year calculation
+//   if (year >= 1700 && year <= 1917) {
+//     // Julian calendar
+//     if (year % 4 === 0) {
+//       isLeapYear = true;
+//     }
+//   } else if (year >= 1919) {
+//     // Gregorian calendar
+//     isLeapYear = findLeapInGregorian(year);
+//   } else if (year === 1918) {
+//     // Special case for 1918 (transition year)
+//     // The 256th day in 1918 is different due to the shift in dates (256 - 230)
+//     return `26.09.${year}`;
+//   }
+
+//   if (isLeapYear) {
+//     date = 256 - first8MonthInDaysLeapYear;
+//   } else {
+//     date = 256 - first8MonthInNormalYear;
+//   }
+
+//   return `${date}.09.${year}`;
+// }
