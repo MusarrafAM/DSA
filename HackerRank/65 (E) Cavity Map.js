2bc5eae647a2f03eb7a370b47dc2d4b1b2f019f7
Musarraf <muhammedmusarraf@gmail.com>
2025-03-07 23:22:56 +0530

    Added Comments for Cavity Map

--- HackerRank/65 (E) Cavity Map.js
+++ HackerRank/65 (E) Cavity Map.js
@@ -1,10 +1,13 @@
-// One of my Favorite problem
+//! One of my Favorite problem
 // Do this again, Did not complete on first try
 
 function cavityMap(grid) {
   // Write your code here
+
+  // Convert the grid into an array of strings to work with
   let gridArray = grid.map((row) => row.split(""));
 
+  //   Iterate over the grid (excluding the border
   for (let i = 1; i < gridArray.length - 1; i++) {
     for (let j = 1; j < gridArray[i].length - 1; j++) {
       let current = gridArray[i][j];
@@ -13,12 +16,14 @@ function cavityMap(grid) {
       let left = gridArray[i][j - 1];
       let right = gridArray[i][j + 1];
 
+      // If the current value is greater than all adjacent values, it's a cavity
       if (current > up && current > down && current > left && current > right) {
         gridArray[i][j] = "X";
       }
     }
   }
 
+  // Convert the result array back into strings
   let finalGrid = gridArray.map((row) => row.join(""));
 
   return finalGrid;
