74765ef83847e81ecfb6c436b8ac317ef0f08638
Musarraf <muhammedmusarraf@gmail.com>
2025-03-21 05:15:03 +0530

    Completed Picking Numbers with better space complexity

--- HackerRank/27. (E) Picking Numbers.js
+++ HackerRank/27. (E) Picking Numbers.js
@@ -1,31 +1,57 @@
 // I could not complete it in first try so do again.
 //If have time Try to solve using 2 pointers after sorting the array.
 
+// new solution with a better space complexity
 function pickingNumbers(a) {
   // Write your code here
-
-  let PickedNumbersListOfList = [];
-  let MaxSizedListLength = 0;
+  let longestSubArray = 0;
 
   a.sort((a, b) => a - b);
 
   for (let i = 0; i < a.length; i++) {
-    let newList = [];
-    newList.push(a[i]);
+    let tempCount = 1;
     for (let j = i + 1; j < a.length; j++) {
-      let difference = Math.abs(a[i] - a[j]);
-      if (difference <= 1) {
-        newList.push(a[j]);
+      let diff = a[j] - a[i];
+      if (diff <= 1) {
+        tempCount++;
+      } else {
+        break;
       }
     }
-    PickedNumbersListOfList.push(newList);
+
+    if (tempCount > longestSubArray) {
+      longestSubArray = tempCount;
+    }
   }
 
-  PickedNumbersListOfList.forEach((arr) => {
-    if (arr.length > MaxSizedListLength) {
-      MaxSizedListLength = arr.length;
-    }
-  });
-
-  return MaxSizedListLength;
+  return longestSubArray;
 }
+
+// function pickingNumbers(a) {
+//   // Write your code here
+
+//   let PickedNumbersListOfList = [];
+//   let MaxSizedListLength = 0;
+
+//   a.sort((a, b) => a - b);
+
+//   for (let i = 0; i < a.length; i++) {
+//     let newList = [];
+//     newList.push(a[i]);
+//     for (let j = i + 1; j < a.length; j++) {
+//       let difference = Math.abs(a[i] - a[j]);
+//       if (difference <= 1) {
+//         newList.push(a[j]);
+//       }
+//     }
+//     PickedNumbersListOfList.push(newList);
+//   }
+
+//   PickedNumbersListOfList.forEach((arr) => {
+//     if (arr.length > MaxSizedListLength) {
+//       MaxSizedListLength = arr.length;
+//     }
+//   });
+
+//   return MaxSizedListLength;
+// }
