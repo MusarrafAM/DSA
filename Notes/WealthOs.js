60cbb1c43ddd89ea541642f7f98692992f99bf59
Musarraf <muhammedmusarraf@gmail.com>
2025-03-27 22:23:32 +0530

    Updated  nextSum problem with new solution

--- Notes/WealthOs.js
+++ Notes/WealthOs.js
@@ -1,16 +1,29 @@
 // WealthOS final Algorithm Question
 // from 1 to 4 it will print the same, after 4 the previous addition and the currecnt number addion will have to print.
 
-function nextSum(num) {
-  let sum = 0;
-  if (num <= 4) {
-    return num;
+//! my new simple answer
+const n = 5;
+let total = 0;
+
+for (let i = 1; i <= n; i++) {
+  total += i;
+  if (i <= 4) {
+    console.log(i);
   } else {
-    for (let i = 1; i <= num; i++) {
-      sum += i;
-    }
+    console.log(total);
   }
-  return sum;
 }
 
-console.log(nextSum(5));
+// function nextSum(num) {
+//   let sum = 0;
+//   if (num <= 4) {
+//     return num;
+//   } else {
+//     for (let i = 1; i <= num; i++) {
+//       sum += i;
+//     }
+//   }
+//   return sum;
+// }
+
+// console.log(nextSum(5));
