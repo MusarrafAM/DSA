943110ad902589c13e283d5f3b31adec40e08db0
Musarraf <muhammedmusarraf@gmail.com>
2025-04-17 18:18:56 +0530

    flippingBits

---
+++ HackerRank/88 (E) Flipping bits.js
@@ -0,0 +1,8 @@
+//! ~n – Bitwise NOT Operator  (0 into 1 , 1 into 0)
+//! >>> operator converts the signed 32-bit integer to an unsigned 32-bit integer.
+// this is just a trick to do this. remember this.
+
+function flippingBits(n) {
+  // Write your code here
+  return ~n >>> 0;
+}
