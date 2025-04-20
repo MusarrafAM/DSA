82be7e96917a5ccc8116156201ff54638b29c85f
Musarraf <muhammedmusarraf@gmail.com>
2025-04-21 00:16:16 +0530

    Added alternative solution

--- HackerRank/80. (E) Minimum Absolute Difference in an Array.js
+++ HackerRank/80. (E) Minimum Absolute Difference in an Array.js
@@ -1,5 +1,6 @@
 //! Completed on Final min wth a fluke, even though this is the perfect answer. Do again.
 
+//! using single loop
 function minimumAbsoluteDifference(arr) {
   // Write your code here
   arr.sort((a, b) => a - b);
@@ -15,3 +16,22 @@ function minimumAbsoluteDifference(arr) {
 
   return minDiff;
 }
+
+//! using double for loop
+// function minimumAbsoluteDifference(arr) {
+//   // Write your code here
+  
+//   let minDif = Infinity
+  
+//   for(let i=0; i<arr.length; i++){
+//       for(let j=i+1; j<arr.length; j++){
+//           let dif = Math.abs(arr[i] - arr[j])
+//           if(dif < minDif){
+//               minDif = dif
+//           }
+//       }
+//   }
+  
+//   return minDif
+
+// }
