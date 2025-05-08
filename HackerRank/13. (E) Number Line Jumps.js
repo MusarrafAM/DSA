f111dcffb708e2f600bfcdd4e215e12acc7b20e2
Musarraf <muhammedmusarraf@gmail.com>
2025-05-09 02:39:47 +0530

    Update NumberLine Jumps answer

--- HackerRank/13. (E) Number Line Jumps.js
+++ HackerRank/13. (E) Number Line Jumps.js
@@ -5,17 +5,14 @@
 function kangaroo(x1, v1, x2, v2) {
   // Write your code here
 
-  if (v1 < v2) {
-    return "NO";
-  }
-
-  if (v1 === v2) {
+  if (v1 <= v2) {
     return "NO";
   }
 
   while (x1 < x2) {
     x1 += v1;
     x2 += v2;
+
     if (x1 === x2) {
       return "YES";
     }
