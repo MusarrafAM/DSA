1cd08c698c3914cc2bf51f06fff57caf1e60d03c
Musarraf <muhammedmusarraf@gmail.com>
2025-05-11 00:37:10 +0530

    Updated Viral Adversing solution

--- HackerRank/34. (E) Viral Advertising.js
+++ HackerRank/34. (E) Viral Advertising.js
@@ -1,14 +1,36 @@
 // Did it on the first try a little tricky
 
+
+// new simple to understand solution
 function viralAdvertising(n) {
   // Write your code here
-  let peoplesLiked = 0;
-  let peopleGotShared = 5;
-
-  for (let i = 0; i < n; i++) {
-    let currentPeopleLiked = Math.floor(peopleGotShared / 2);
-    peoplesLiked += currentPeopleLiked;
-    peopleGotShared = currentPeopleLiked * 3;
+  let totalLikes = 0
+  let curPeoples = 5
+  
+  for(let i=1; i<=n; i++){
+      
+      let curLikes = Math.floor(curPeoples / 2)
+      totalLikes += curLikes
+      
+      
+      curPeoples = curLikes * 3
   }
-  return peoplesLiked;
+  
+  return totalLikes
+  
 }
+
+// function viralAdvertising(n) {
+//   // Write your code here
+//   let peoplesLiked = 0;
+//   let peopleGotShared = 5;
+
+//   for (let i = 0; i < n; i++) {
+//     let currentPeopleLiked = Math.floor(peopleGotShared / 2);
+//     peoplesLiked += currentPeopleLiked;
+//     peopleGotShared = currentPeopleLiked * 3;
+//   }
+//   return peoplesLiked;
+// }
+
+
